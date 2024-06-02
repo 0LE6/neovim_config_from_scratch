80d9af34fb228a56d893cf635b10850bb63c8a19
0LE6 <kharenkoleg@gmail.com>
2024-06-02 15:01:44 +0200

    configurando nerd tree (FIX BUG)

--- lua/0LE6/init.lua
+++ lua/0LE6/init.lua
@@ -1,6 +1,6 @@
 require("0LE6.remap")
 require("0LE6.lazy_init")
-
+require("0LE6.settings")
 
 -- TODO: Prueba, borrar luego
 print("Hola desde 0LE6")
--- lua/0LE6/lazy/nvim_tree.lua
+++ lua/0LE6/lazy/nvim_tree.lua
@@ -1,15 +1,43 @@
 -- Aquí realizamos la configuración correspondiente para tener la versión "cool" de NerdTree para NeoVim, que consiste básicamente en la disposición visual complementaria de la organización de carpetas y sus contenidos de archivos.
 --
 -- https://github.com/nvim-tree/nvim-tree.lua/wiki/Installation
+
+local function my_on_attach(bufnr)
+	local api = require 'nvim-tree.api'
+
+	local function opts(desc)
+		return {
+			desc = 'nvim-tree: ' .. desc,
+			buffer = bufnr, 
+			noremap = true,
+			silent = true, 
+			nowait = true
+		}
+	end
+
+	-- El mappeo de los shortcuts personalizados se realizan en base a su API
+	-- Presionar el leader (espacio) junto a una "a" -> lo abrimos
+	vim.keymap.set('n', '<leader>a', api.node.open.edit(), opts('Open'))
+	
+	-- Presionar el leader (espacio) junto a una "q" -> lo... quitamos lol
+	vim.keymap.set('n', '<leader>q', api.tree.close(), opts('Close'))
+	-- ejemplo	
+	-- vim.keymap.set('n', '<C-t>', api.tree.change_root_to_parent,        opts('Up'))
+end
+
+
+
 return {
 	'nvim-tree/nvim-tree.lua',
-	version = '*',
 	lazy = false,
 	dependencies = {
 		'nvim-tree/nvim-web-devicons',
 	},
 
 	config = function()
-		require('nvim-tree').setup {}
+		require('nvim-tree').setup {
+			-- Le pasamos la configuración que realizamos antes
+			on_attach = my_on_attach,
+		}
 	end
 }
