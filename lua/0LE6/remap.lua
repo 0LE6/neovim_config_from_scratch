94c5231a779f345dc1d22f931fb64dfa9e61be3e
Oleg <kharenkoleg@gmail.com>
2024-06-05 20:08:25 +0200

    add: mappeo de tabulación de más de una línea en V

--- lua/0LE6/remap.lua
+++ lua/0LE6/remap.lua
@@ -5,3 +5,9 @@ vim.g.mapleader = " "
 -- Mientras estoy en modo "Normal", si presiono la tecla líder seguido de "ew" ejecuta :Ex
 vim.keymap.set("n", "<leader>ew", vim.cmd.Ex)
 
+-- Configuración para permitir la indentación con Tab en modo visual
+-- En modo V -> si selecciono linea/s y hago Tab o Shift+Tab, entonces tabulamos hacia delante o atrás.
+vim.keymap.set('v', '<Tab>', '>gv', {noremap = true, silent = true})
+vim.keymap.set('v', '<S-Tab>', '<gv', {noremap = true, silent = true})
+
+
