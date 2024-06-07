25626644e05cdb226e0e15350e67081124c78bc7
Oleg <kharenkoleg@gmail.com>
2024-06-07 22:22:42 +0200

    add: mappeo de cambio de tabs en modo normal mediante Tab & S-Tab

--- lua/0LE6/remap.lua
+++ lua/0LE6/remap.lua
@@ -10,4 +10,9 @@ vim.keymap.set("n", "<leader>ew", vim.cmd.Ex)
 vim.keymap.set('v', '<Tab>', '>gv', {noremap = true, silent = true})
 vim.keymap.set('v', '<S-Tab>', '<gv', {noremap = true, silent = true})
 
+-- Mapeo para avanzar a la siguiente pestaña con Tab
+vim.keymap.set('n', '<Tab>', ':tabnext<CR>', {noremap = true, silent = true})
+
+-- Mapeo para ir a la pestaña anterior con Shift+Tab
+vim.keymap.set('n', '<S-Tab>', ':tabprevious<CR>', {noremap = true, silent = true})
 
