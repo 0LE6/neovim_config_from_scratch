ea4a40be0ce7eb879b383cd4e465f4750e88b5d8
Oleg <kharenkoleg@gmail.com>
2024-06-20 21:31:18 +0200

    update: config autopairs plugin. Ahora completa también <>

--- lua/0LE6/lazy/autopairs.lua
+++ lua/0LE6/lazy/autopairs.lua
@@ -2,7 +2,19 @@
 return {
     'windwp/nvim-autopairs',
     event = "InsertEnter",
-    config = true,
-    -- use opts = {} for passing setup options
-    -- this is equalent to setup({}) function
+    config = function()
+        local npairs = require('nvim-autopairs')
+        local Rule = require('nvim-autopairs.rule')
+
+        npairs.setup({
+            disable_filetype = { "TelescopePrompt", "vim" },
+            enable_check_bracket_line = false,
+            check_ts = true,  -- Habilitar integración con treesitter
+        })
+
+        -- Añadir reglas adicionales para todos los lenguajes
+        npairs.add_rules({
+            Rule("<", ">")
+        })
+    end
 }
